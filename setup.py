f2eab39e3673095e95dc62ece981ec551be66069
zackey-heuristics <yamazaki.keita.xa@alumni.tsukuba.ac.jp>
2025-03-26 04:25:03 +0900

    Fix entry point command for console script in setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     description="holehe allows you to check if the mail is used on different sites like twitter, instagram , snapchat and will retrieve information on sites with the forgotten password function.",
     include_package_data=True,
     url='http://github.com/megadose/holehe',
-    entry_points = {'console_scripts': ['holehe = holehe.json_output:main']},
+    entry_points = {'console_scripts': ['holehe-json = holehe.json_output:main']},
     classifiers=[
         "Programming Language :: Python",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
